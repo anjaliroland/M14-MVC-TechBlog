4d891a2368c23f8e4aaa168cced5f1895f4e9055
anjaliroland <anjaligroland@gmail.com>
2023-05-04 22:08:54 -0700

    connected cotrollers

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const homeRoutes = require('./homeRoutes');
+const dashRoutes = require('./dashRoutes');
+const apiRoutes = require('./api');
+
+router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+router.use('/api', apiRoutes);
+
+module.exports = router;
