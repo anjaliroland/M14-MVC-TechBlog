3bb05d1369bfe58a7ae2d36d7af4754290cbeecf
anjaliroland <anjaligroland@gmail.com>
2023-05-06 21:25:01 -0700

    added login, log out, and sign up eventlisteners

---
+++ public/js/signup.js
@@ -0,0 +1,22 @@
+const signupForm = document.querySelector("#signup-form");
+signupForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    const userObj = {
+        email:document.querySelector("#signup-email").value,
+        name:document.querySelector("#signup-username").value,
+        password:document.querySelector("#signup-password").value,
+    }
+    fetch("/api/users",{
+        method:"POST",
+        body:JSON.stringify(userObj),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    }).then(res=>{
+        if(res.ok){
+           location.href = "/"
+        } else {
+            alert("trumpet sound")
+        }
+    })
+})
