8685d7953b0b945cf636248244abcee7f2a8c1b1
anjaliroland <anjaligroland@gmail.com>
2023-05-08 20:57:00 -0700

    added comments

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 
-// 
+// signup new user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 });
 
 
-//
+// login user
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -44,7 +44,7 @@ router.post('/login', async (req, res) => {
 });
 
 
-//
+// logout user
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
